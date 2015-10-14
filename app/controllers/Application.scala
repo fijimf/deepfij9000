5aa40cd5026d7ecf10a13ce8b49dec34b540dedb
fijimf <fijimf@gmail.com>
2015-10-13 23:22:33 -0400

    Fleshing out analysis

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,15 +13,29 @@ import scala.concurrent.duration._
 
 import scala.concurrent.{Future, ExecutionContext}
 
-class Application @Inject() (@Named("team-load-actor") teamLoad: ActorRef)
-                            (implicit ec: ExecutionContext) extends Controller {
-  implicit val timeout = Timeout(5.seconds)
+class Application @Inject()(@Named("team-load-actor") teamLoad: ActorRef)
+                           (implicit ec: ExecutionContext) extends Controller {
+  implicit val timeout = Timeout(105.seconds)
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   def other = Action.async {
-    (teamLoad ? "Jim").map(s=> Ok(views.html.index(s.toString)))
+    (teamLoad ? "Jim").map(s => Ok(views.html.index(s.toString)))
+  }
+
+  def scrape = {
+    Action.async {
+      List(2015, 2014, 2013, 2012).foreach(yr=> {
+         (teamLoad ? yr).mapTo[(Map[Int, String],Map[Int, String])].map(tup=>{
+
+         })
+      }
+
+
+      future.map(r => Ok(r.toString))
+    }
   }
 
   def team = Action {
@@ -30,11 +44,11 @@ class Application @Inject() (@Named("team-load-actor") teamLoad: ActorRef)
       "Georgetown",
       None,
       Some("Hoyas"),
-      Some( LogoUrls(
+      Some(LogoUrls(
         Some("http://i.turner.ncaa.com/dr/ncaa/ncaa7/release/sites/default/files/images/logos/schools/w/william-mary.40.png"),
         Some("http://i.turner.ncaa.com/dr/ncaa/ncaa7/release/sites/default/files/images/logos/schools/w/william-mary.70.png")
       )
-      ),Some(Colors(Some("#116633"),None )))))
+      ), Some(Colors(Some("#116633"), None)))))
 
   }
 
--- app/modules/Scraping.scala
+++ app/modules/Scraping.scala
@@ -2,12 +2,14 @@ package modules
 
 import java.io.{StringReader, Reader}
 
-import akka.actor.{ActorSystem, Actor}
+import akka.actor.{ActorRef, ActorSystem, Actor}
 import com.google.inject.{Inject, AbstractModule}
 import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 import play.api.libs.concurrent.AkkaGuiceSupport
-import play.api.libs.ws.{WSResponse, WS, WSClient}
+import play.api.libs.ws.{WSRequest, WSResponse, WS, WSClient}
 
+import scala.collection.immutable
+import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 import scala.xml.{NodeSeq, InputSource, Node}
 import scala.xml.parsing.NoBindingFactoryAdapter
@@ -22,38 +24,62 @@ object HTML {
 }
 
 class TeamLoadActor @Inject()(ws: WSClient) extends Actor {
+
   import scala.concurrent.ExecutionContext.Implicits.global
-  val responseHandler: (Try[WSResponse]) => Map[String, String] = {
-    case Success(response) =>
-      HTML.loadString(response.body) match {
-        case Success(htmlNode) =>
-          val seq: NodeSeq = htmlNode \\ "li" \ "a"
-          println(seq.size)
-          val filter: NodeSeq = seq.filter(_.attribute("href").flatMap(_.headOption).exists(_.text.startsWith("/schools/")))
-          val confMap = filter.map(n => {
-            val id = n.attribute("href").flatMap(_.headOption).map(_.text.replace("/schools/", ""))
-            val name: String = (n \ "span").text
-            id -> name
-          }).filter(tup => tup._1.isDefined && tup._1.get.length > 1).map(tup => tup._1.get -> tup._2)
-          confMap.foreach(n => println(n))
-          confMap.toMap
-        case Failure(ex) => Map.empty
-      }
-    case Failure(ex) =>
-      println(ex.getMessage)
-      Map.empty[String, String]
+
+  def extractMaps(htmlNode: Node): (Map[Int, String],Map[Int, String]) = {
+    val teamMap = extractTeamMap(htmlNode)
+
+    val confMap = extractConferenceMap(htmlNode)
+    confMap.foreach(n => println(n))
+    teamMap.foreach(n=> println(n))
+    (teamMap, confMap)
   }
 
+  def extractConferenceMap(htmlNode: Node): Map[Int, String] = {
+    (htmlNode \\ "li" \ "a").filter(_.attribute("href").flatMap(_.headOption).exists(_.text.startsWith("javascript:changeConference"))).map(n => {
+      val id = n.attribute("href").flatMap(_.headOption).map(_.text.replace("javascript:changeConference(", "").replace(");", "").toDouble.toInt)
+      id -> n.text
+    }).filter(_._1.isDefined).map((tup: (Option[Int], String)) => (tup._1.get, tup._2)).toMap
+
+  }
+
+  def extractTeamMap(htmlNode: Node): Map[Int, String] = {
+    (htmlNode \\ "td" \ "a").map(n => {
+      val id = n.attribute("href").flatMap(_.headOption).map(_.text.replaceAll( """/team/index/\d+\?org_id=""", "").toInt)
+      id -> n.text
+    }).filter(_._1.isDefined).map(tup => tup._1.get -> tup._2).toMap
+  }
 
   override def receive: Receive = {
-
     case year: Int =>
-      ws.url("http://stats.ncaa.org/team/inst_team_list?academic_year=" + year + "&conf_id=-1&division=1&sport_code=MBB").get().onComplete(responseHandler)
-
-
+      val mySender = sender()
+      ws.url(createURL(year)).get().onComplete {
+        case Success(response) =>
+          HTML.loadString(response.body) match {
+            case Success(htmlNode) =>
+              mySender ! extractMaps(htmlNode)
+            case Failure(ex) => sender ! "Failed with exception " + ex.getMessage
+          }
+        case Failure(ex) => sender ! "Failed with exception " + ex.getMessage
+      }
+    case (year:Int, conference:Int) =>
+      val mySender = sender()
+      ws.url(createURL(year, conference)).get().onComplete {
+        case Success(response) =>
+          HTML.loadString(response.body) match {
+            case Success(htmlNode) =>
+              mySender ! extractTeamMap(htmlNode)
+            case Failure(ex) => sender ! "Failed with exception " + ex.getMessage
+          }
+        case Failure(ex) => sender ! "Failed with exception " + ex.getMessage
+      }
     case _ =>
       println("Unexpected message")
+  }
 
+  def createURL(year: Int, confId: Int = -1): String = {
+    "http://stats.ncaa.org/team/inst_team_list?academic_year=" + year + "&conf_id="+confId+"&division=1&sport_code=MBB"
   }
 }
 
