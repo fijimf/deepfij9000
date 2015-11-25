bf45cd2743a1f60422c3bd1e37c7e69a5f772ee0
fijimf <fijimf@gmail.com>
2015-11-24 23:50:50 -0500

    Loading games

--- app/modules/scraping/ScrapingActor.scala
+++ app/modules/scraping/ScrapingActor.scala
@@ -59,26 +59,32 @@ class ScrapingActor @Inject()(ws: WSClient) extends Actor {
 
 
 def handleJsonScrape(r: JsonScrapeRequest[_]): Unit = {
-    val mySender = sender()
-    logger.info("Requesting " + r.url)
-    ws.url(r.url).get().onComplete {
-      case Success(response) =>
-        if (response.status == 200) {
-          logger.info("%d - %s (%d bytes)".format(response.status, response.statusText, response.body.length))
-        } else {
-          logger.warn("%d - %s (%s )".format(response.status, response.statusText, r.url))
-        }
-        Try {Json.parse(r.preProcessBody(response.body))} match {
+  val mySender = sender()
+  logger.info("Requesting " + r.url)
+  ws.url(r.url).get().onComplete {
+    case Success(response) =>
+      if (response.status == 200) {
+        logger.info("%d - %s (%d bytes)".format(response.status, response.statusText, response.body.length))
+      } else {
+        logger.warn("%d - %s (%s )".format(response.status, response.statusText, r.url))
+      }
+      if (response.body.length < 10) {
+        mySender ! "Failed with empty body"
+      } else {
+        Try {
+          Json.parse(r.preProcessBody(response.body))
+        } match {
           case Success(js) =>
             mySender ! r.scrape(js)
           case Failure(ex) =>
-            logger.error("Error parsing response:\n" + ex.getMessage+" "+response.body )
-            sender ! "Failed with exception " + ex.getMessage
+            logger.error("Error parsing response:\n" + ex.getMessage + " " + response.body)
+            mySender ! "Failed with exception " + ex.getMessage
         }
-      case Failure(ex) =>
-        logger.error("Get failed", ex)
-        sender ! "Failed with exception " + ex.getMessage
-    }
+      }
+    case Failure(ex) =>
+      logger.error("Get failed", ex)
+      mySender ! "Failed with exception " + ex.getMessage
   }
+}
 }
 
