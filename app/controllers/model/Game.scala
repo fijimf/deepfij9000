4615dbff9fb089caa5040419e801d0360bb56ac8
fijimf <fijimf@gmail.com>
2015-11-25 10:26:55 -0500

    Slight mod to game info

--- app/controllers/model/Game.scala
+++ app/controllers/model/Game.scala
@@ -2,20 +2,26 @@ package controllers.model
 
 import org.joda.time.LocalDateTime
 
-case class Game(date: LocalDateTime, homeTeam: Team, awayTeam: Team, result: Option[Result], isNeutral: Boolean = false, isConfTournament: Option[Boolean]) {
-  def opponent(team:Team) =  team match {
-    case `homeTeam` => Some(awayTeam)
-    case `awayTeam` => Some(homeTeam)
+object Game {
+  def fromGameData(gd:GameData):Game = {
+    
+  }
+}
+
+case class Game(date: LocalDate, homeTeamKey: String, awayTeamKey: String, result: Option[Result], isNeutral: Boolean = false, isConfTournament: Option[Boolean], ncaaTourneyInfo:Option[TourneyInfo], location:Option[String], periods:Int) {
+  def opponent(team:String) =  team match {
+    case `homeTeamKey` => Some(awayTeamKey)
+    case `awayTeamKey` => Some(homeTeamKey)
     case _ => None
   }
 
   def score(team: Team): Option[Int] = result.flatMap(res => team match {
-    case `homeTeam` => Some(res.homeScore)
-    case `awayTeam` => Some(res.awayScore)
+    case `homeTeamKey` => Some(res.homeScore)
+    case `awayTeamKey` => Some(res.awayScore)
     case _ => None
   })
 
-  def isWinner(team: Team): Boolean = result.exists(res => (team == homeTeam && res.homeScore > res.awayScore) || (team == awayTeam && res.awayScore > res.homeScore))
+  def isWinner(team: String): Boolean = result.exists(res => (team == homeTeamKey && res.homeScore > res.awayScore) || (team == awayTeamKey && res.awayScore > res.homeScore))
 
-  def isLoser(team: Team): Boolean = result.exists(res => (team == homeTeam && res.homeScore < res.awayScore) || (team == awayTeam && res.awayScore < res.homeScore))
+  def isLoser(team: String): Boolean = result.exists(res => (team == homeTeamKey && res.homeScore < res.awayScore) || (team == awayTeamKey && res.awayScore < res.homeScore))
 }
